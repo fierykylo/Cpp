06cf830999f82ea68eb3d986fbf5d42d8ecf9c9a
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-09-26 18:01:29 +0530

    Add files via upload

---
+++ oops/oops7.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// this is for inline functions 
+
+inline int add(int a, int b)
+{
+    return a + b;
+}
+int main(void)
+{
+    int a = 10;
+    int b = 20; 
+    add(a, b);
+}
